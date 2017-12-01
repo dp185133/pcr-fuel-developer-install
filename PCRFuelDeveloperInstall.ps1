6445c7dfe6edbd3f46c5e6d622eddfff400aa5ee
MattPlacek <dp185133@ncr.com>
2017-12-01 06:13:22 +0000

    add PS3 check; allow selection of multiple configurations

--- PCRFuelDeveloperInstall.ps1
+++ PCRFuelDeveloperInstall.ps1
@@ -7,8 +7,10 @@ echo '=================================================================='
 echo ''
 start-sleep 1
 
+
 # Steps.
-# 1. Install Git4 win
+# 1a. Ensure we have PowerShell 3
+# 1b. Install Git4 win
 # 2. Pull PSBabushka from dp185133 almgit repo
 # 3. Pull NCR/PCR staging directory with PSBabushka scripts
 # 4. Set execution policy
@@ -25,8 +27,32 @@ function Is-Installed( $program ) {
 }
 
 
+
 ##################################################################
-# Verify Git is installed and create shortcut function for this script
+# Verify PS 3.0 or greater is installed
+echo "$sep Checking for PowerShell 3.0 at least"
+
+if ($PSVersionTable.psversion.major -lt 3) {
+    echo "$sep Installing PowerShell 3"
+
+    $tempexe = [System.IO.Path]::GetTempFileName() + ".msu"
+    # https://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.0-KB2506146-x64.msu
+    (new-object Net.WebClient).DownloadFile(
+        "https://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.1-KB2506143-x64.msu",
+        $tempexe)
+    start-sleep 1.5
+    start-process $tempexe -wait
+    remove-item -force $tempexe
+
+    read-host -prompt "$sep PowerShell updated; please re-run PCRFuelDeveloperInstall`nPress enter to finish..."
+    throw "Powershell updated - need to restart"
+}
+
+
+##################################################################
+# Verify Git Is Installed and create shortcut function for this script.
+# This is included with the installer because the main download is on
+# amazonaws and gives a permission denied to invoke-webrequest.
 echo "$sep Checking for Git"
 $psmodulepath = "$($env:USERPROFILE)\Documents\WindowsPowershell\Modules"
 if (-not $(is-installed 'git version 2.14')) {
@@ -53,25 +79,6 @@ if (-not $?) {
 
 $fuelinstpath = $PWD
 
-##################################################################
-# Verify PS 3.0 or greater is installed
-echo "$sep Checking for PowerShell 3.0 at least"
-
-if ($PSVersionTable.psversion.major -lt 3) {
-    echo "$sep Installing PowerShell 3"
-
-    $tempexe = [System.IO.Path]::GetTempFileName() + ".msu"
-    # https://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.0-KB2506146-x64.msu
-    (new-object Net.WebClient).DownloadFile(
-        "https://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.1-KB2506143-x64.msu",
-        $tempexe)
-    start-sleep 1.5
-    start-process $tempexe -wait
-    remove-item -force $tempexe
-
-    read-host -prompt "$sep PowerShell updated; please re-run PCRFuelDeveloperInstall`nPress enter to finish..."
-    throw "Powershell updated - need to restart"
-}
 
 ##################################################################
 # Verify PSBabushka is installed and module is in our working space
@@ -105,6 +112,33 @@ if (-not $(test-path $staging/pcrfuelbabushka)) {
     popd
 }
 
+function make-configuration( $name, $description, $configname ) {
+    $config = [pscustomobject] @{ 'Name' = $name; 'Description' = $description; }
+    Add-Member -MemberType ScriptMethod -name "getSymbolicConfigName" `
+      -value ([scriptblock]::Create("`"$configname`"")) -inputObject $config
+    $config
+}
+
+
+$selectedConfiguration = `
+  (make-configuration 'Full Fuel Developer' 'All prerequisites for typical PCR Fuel Developer machine.' 'ncr-pcr-fuel-dev'),
+  (make-configuration 'Only WEC7 Development' 'Prerequisites for only WEC7/Panther+x86 compilation environment.' 'ncr-pcr-wec7') |
+  out-gridview -title "PCRFuelDeveloperInstall: Select Configuration" -outputmode single
+
+#  (make-configuration 'WEC7 SDK Only' 'Only Install Panther/WEC7 SDK and patches.' 'panther-wec7-with-patches') |
+
+if (-not $selectedConfiguration) {
+    Write-Output "No configuration selected; exiting";
+    throw "no configuration selected; nothing to do!"
+}
+
+
+. "$PSScriptRoot/Show-MessageBox.ps1"
+$promptResponse = Show-MessageBox -title "Install $($selectedConfiguration.name) Tools?" -Msg `
+  "Press 'Yes' to install all prerequisites automatically;`nPress 'No' to prompt before making changes for each prerequisite.`nPress 'Cancel' to abort." -YesNoCancel
+
+if ($promptResponse -eq 'Cancel') { throw "Installation cancelled" }
+
 
 echo "$sep Checking NCR/PCR Fuel Developer Prerequisites $sep Please assist in tools installation as needed."
 
@@ -114,10 +148,25 @@ import-module psbabushka
 
 $env:PATH_BABUSHKA = "$staging/pcrfuelbabushka"
 
+$configName = $selectedConfiguration.getSymbolicConfigName()
 try {
-    invoke-psbabushka ncr-pcr-fuel-dev
+    if ($promptResponse -eq 'Yes') {
+        invoke-psbabushka $configName
+    } else {
+        invoke-psbabushka $configName -confirm {
+            param($name,$description,$block)
+            $blockString = $block.ToString()
+            $blockDesc = $blockString.Substring(0, [Math]::Min($blockString.length, 3200)) # only show max of about 80chars*40 linese
+            $response = Show-MessageBox -Title "Install `"$name`"?" -YesNoCancel -Msg "Attempting to configure package `"$name`".`nActions to be performed:`n$blockDesc"
+            if ($response -eq 'Cancel') {
+                Write-Output "Aborting installation due to user cancel"
+                throw "Install aborted!!"
+            }
+            ($response -eq 'Yes')
+        }
+    }
 } catch {
-    echo "Error invoking PSBabushka to configure 'ncr-pcr-fuel-dev'."
+    echo "Error invoking PSBabushka to configure '$($selectedConfiguration.Name)' ($configName)."
 }
 
 
